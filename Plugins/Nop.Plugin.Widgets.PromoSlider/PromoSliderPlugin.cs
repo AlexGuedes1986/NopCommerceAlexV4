3a6fbe63dc6521f2693d193dcd5f35f6bacf811b
Alexander Guedes <alexander.guedes1986@gmail.com>
2018-04-25 21:37:26 -0400

    more changes

--- Plugins/Nop.Plugin.Widgets.PromoSlider/PromoSliderPlugin.cs
+++ Plugins/Nop.Plugin.Widgets.PromoSlider/PromoSliderPlugin.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Routing;
 using Nop.Core.Plugins;
 using Nop.Plugin.Widgets.PromoSlider.Infraestructure;
 using Nop.Services.Cms;
+using Nop.Web.Framework.Menu;
 
 namespace Nop.Plugin.Widgets.PromoSlider
 {
-    public class PromoSliderPlugin : BasePlugin, IWidgetPlugin
+    public class PromoSliderPlugin : BasePlugin, IWidgetPlugin, IAdminMenuPlugin
     {
 	    private PromoSliderObjectContext _context;
 
@@ -39,5 +41,46 @@ namespace Nop.Plugin.Widgets.PromoSlider
 	    {
 		    throw new NotImplementedException();
 	    }
-    }
+
+		public void ManageSiteMap(SiteMapNode rootNode)
+		{
+			var menuItem = new SiteMapNode()
+			{
+				SystemName = "YourCustomSystemName",
+				Title = "Promo Slider",
+				ControllerName = "ControllerName",
+				ActionName = "List",
+				Visible = true,
+				RouteValues = new RouteValueDictionary() { { "area", null } },
+			};
+
+			var createUpdateNode = new SiteMapNode()
+			{
+				SystemName = "YourCustomSystemName",
+				Title = "New Slider",
+				ControllerName = "ControllerName",
+				ActionName = "List",
+				Visible = true,
+				RouteValues = new RouteValueDictionary() { { "area", null } },
+			};
+
+			var manageSliders = new SiteMapNode()
+			{
+				SystemName = "YourCustomSystemName",
+				Title = "Manage Sliders",
+				ControllerName = "ControllerName",
+				ActionName = "List",
+				Visible = true,
+				RouteValues = new RouteValueDictionary() { { "area", null } },
+			};
+
+			menuItem.ChildNodes.Add(createUpdateNode);
+			menuItem.ChildNodes.Add(manageSliders);
+			var pluginNode = rootNode.ChildNodes.FirstOrDefault(x => x.SystemName == "Third party plugins");
+			if (pluginNode != null)
+				pluginNode.ChildNodes.Add(menuItem);
+			else
+				rootNode.ChildNodes.Add(menuItem);
+		}
+	}
 }
